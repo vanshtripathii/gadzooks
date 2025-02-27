18a7af1efd89dcf137096bcb2d43d7b21083486a
Vansh Tripathi <137267169+vanshtripathii@users.noreply.github.com>
2025-02-28 00:29:40 +0530

    Add files via upload

---
+++ GADzooks/pg.sql
@@ -0,0 +1,11 @@
+/gadzooks-project
+│-- /public
+│   │-- index.html
+│   │-- denim.html
+│   │-- streetknit.html
+│   │-- styles.css
+│   │-- script.js
+│-- /server
+│   │-- db.js
+│   │-- server.js
+│-- package.json

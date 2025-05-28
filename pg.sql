fcaf796c73dc0e870369ee5c165d440aa913010b
Vansh Tripathi <137267169+vanshtripathii@users.noreply.github.com>
2025-05-28 18:36:20 +0530

    Add files via upload

---
+++ pg.sql
@@ -0,0 +1,11 @@
+/GADzooks
+│-- /public
+│   │-- index.html
+│   │-- denim.html
+│   │-- streetknit.html
+│   │-- styles.css
+│   │-- script.js
+│-- /server
+│   │-- db.js
+│   │-- server.js
+│-- package.json
